4631f099cd6fbaec1a06ddcd0019e2f651cf8e5e
Ali Asif <aliasif1171@gmail.com>
2024-05-10 11:41:55 +0500

    Fixed Query 4

--- QUERIES/Queries.sql
+++ QUERIES/Queries.sql
@@ -14,34 +14,26 @@ ON MEMBER.membership_id = RES1.membership_id
 WHERE Member.dietPlan_id = 1
 
 -- Query 4
-SELECT COUNT(RES4.membership_id) AS [Total Machine Users]
-FROM EXERCISE
+SELECT COUNT(member_id) AS [No. of Members]
+FROM DAY
 JOIN (
-	SELECT EXERCISE_DAY.exercise_id, EXERCISE_DAY.day_id, RES3.membership_id
-	FROM EXERCISE_DAY
+	SELECT member_id, workoutPlan_id, [Equipment Name]
+	FROM MEMBER
 	JOIN (
-		SELECT day_id, RES2.membership_id
-		FROM DAY
+		SELECT membership_id, RES1.name AS [Equipment Name]
+		FROM MEMBERSHIP
 		JOIN (
-			SELECT workoutPlan_id, RES1.membership_id
-			FROM MEMBER
-			JOIN (
-				SELECT GYM.name AS 'Gym_Name', MEMBERSHIP.membership_id
-				FROM GYM
-				JOIN Membership
-				ON GYM.gym_id = Membership.gym_id
-				WHERE GYM.name = 'SYN'
-			) AS RES1
-			ON MEMBER.membership_id = RES1.membership_id
-		) AS RES2
-		ON DAY.workoutPlan_id = RES2.workoutPlan_id
-		WHERE DAY.dayOfWeek = 'Saturday'
-	) AS RES3
-	ON EXERCISE_DAY.day_id = RES3.day_id
-) AS RES4
-ON EXERCISE.exercise_id = RES4.exercise_id
-WHERE equipment_name LIKE '%Machine%'
-
+			SELECT *
+			FROM EQUIPMENT
+			WHERE gym_id = 1
+		) AS RES1
+		ON Membership.gym_id = RES1.gym_id
+	) AS RES2
+	ON MEMBER.membership_id = RES2.membership_id
+) AS RES3
+ON DAY.workoutPlan_id = RES3.workoutPlan_id
+WHERE dayOfWeek = 'Saturday' AND [Equipment Name] = 'Cable Machine'
+	
 -- Query 10
 SELECT RES1.name AS [Gym Name], COUNT(membership_id) AS [Number of Members]
 FROM (
