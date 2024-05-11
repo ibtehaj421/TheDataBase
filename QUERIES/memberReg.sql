15e8fc1f35e33bd89835869da6ff63dda616f8c8
ibtehaj421 <114730186+ibtehaj421@users.noreply.github.com>
2024-05-11 15:02:23 +0500

    Update memberReg.sql
    added the add diet plan feature

--- QUERIES/memberReg.sql
+++ QUERIES/memberReg.sql
@@ -22,3 +22,39 @@ END
 DROP PROCEDURE RegisterMemberDetails
 CALL RegisterMemberDetails('Ibtehaj','ibtehaj@gmail.com','password','Student','Gym Revolution') ;
 EXEC RegisterMemberDetails @usrnem = 'ibtehaj',@email='ibtehaj@gmail.com',@pass='password',@type='Student',@gname='Gym Revolution';
+
+
+--add diet plan change according to requirements
+
+CREATE PROCEDURE AddDietPlan(
+	@memberID VARCHAR(7),
+	@nutri1 VARCHAR(30),
+	@nutri2 VARCHAR(30),
+	@type VARCHAR(30),
+	@objective VARCHAR(30),
+	@difficulty VARCHAR(30),
+	@guideline VARCHAR(100)
+	)
+AS 
+BEGIN
+	DECLARE @nutriID VARCHAR(7);
+	DECLARE @nutriID2 VARCHAR(7);
+	DECLARE @dietID INT;
+	DECLARE @mealID INT;
+	DECLARE @nutriCal INT;
+	DECLARE @nutriCal2 INT;
+	SET @nutriID = (SELECT nutrition_id FROM NUTRITION WHERE name = @nutri1);
+	SET @nutriID2 = (SELECT nutrition_id FROM NUTRITION WHERE name = @nutri2);
+	SET @dietID = (SELECT COUNT(*)+1 FROM DIET_PLAN);
+	SET @mealID = (SELECT COUNT(*)+1 FROM MEAL);
+	SET @nutriCal = (SELECT calories FROM NUTRITION WHERE name = @nutri1);
+	SET @nutriCal2 = (SELECT calories FROM NUTRITION WHERE name = @nutri2);
+	INSERT INTO MEAL (meal_id,nutrition_id) VALUES (CONCAT('ML',@mealID),@nutriID);
+	INSERT INTO meal_nutrient(nutrient_id,meal_id,calorie) VALUES (@nutriID,CONCAT('ML',@mealID),@nutriCal);
+	INSERT INTO meal_nutrient(nutrient_id,meal_id,calorie) VALUES (@nutriID2,CONCAT('ML',@mealID),@nutriCal2);
+	INSERT INTO DIET_PLAN(dietPlan_id,meal_id,type,objective,guidelines,difficulty_level) VALUES (CONCAT('D',@dietID),CONCAT('ML',@mealID),@type,@objective,@guideline,@difficulty);
+	INSERT INTO MEMBER_DIETS(member_id,diet_id) VALUES (@memberID,CONCAT('D',@dietID));
+END
+DROP PROCEDURE AddDietPlan
+
+EXEC AddDietPlan @memberID='M1',@nutri1='Omega-3' ,@nutri2='Creatine' ,@type='Lunch' ,@objective='General Health' ,@difficulty='Easy' ,@guideline='IDK man use if you want to' ;
